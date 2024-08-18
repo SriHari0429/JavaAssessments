c4e899cb672ca08064c6d3d4a4dd2adc00a1a611
HARI NAIDU <sriharinaidu951@gmail.com>
2024-08-18 12:31:37 +0530

    Small modifications

--- rewards-converter-flow/src/main/java/RewardsConverter.java
+++ rewards-converter-flow/src/main/java/RewardsConverter.java
@@ -1,27 +1,4 @@
 import java.util.Scanner;
-class RewardValue{
-    private double cashValue;
-    private double milesValue;
-
-    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
-
-    public RewardValue(double cashValue){
-        this.cashValue=cashValue;
-        this.milesValue= cashValue/MILES_TO_CASH_CONVERSION_RATE   ;
-    }
-    public RewardValue(double milesValue,boolean isMiles){
-        this.milesValue=milesValue;
-        this.milesValue= milesValue*MILES_TO_CASH_CONVERSION_RATE   ;
-    }
-    public double getCahValue(){
-        return cashValue;
-    }
-    public double getMilesValue(){
-        return milesValue;
-    }
-
-}
-
 
 public class RewardsConverter {
     public static void main(String[] args) {
